75ec228e3e4d88ebaf57da7dbfa22e915c291cf6
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2019-11-12 22:18:51 +0530

    Initial upload

---
+++ anykernel.sh
@@ -0,0 +1,30 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+kernel.string=WOLF KERNEL
+do.devicecheck=1
+do.modules=1
+do.cleanup=1
+do.system=1
+do.initd=1
+do.cleanuponabort=0
+device.name1=rosy
+
+# shell variables
+block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+
+## end install
+
